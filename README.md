1f226b8d77f84ba44e1bedc83581cb8b0c254a11
Omar Meharab <omarmeharab81@gmail.com>
2021-03-13 11:51:13 +0600

    nearly done

--- README.md
+++ README.md
@@ -1,34 +1,6 @@
-data will be given in object access with teams${index}
-
-background top banner
-slice to load 15 teams.
-show with card.
-badge
-team name
-sport type
-gender
-button explore fontawesome arrow sign right.
-
-top banner with dynamic badge.
-detail page will contain
-card with name
-icon established year
-icon country
-icon sport type
-icon gender
-gender conditional image.
-description outside of card.
-also footer with social media links
-if there is a missing social media then show a component saying this team does not have {media}. try other ones.
-
-team name: strTeam
-established year: intFormedYear
-sports: strSport
-team logo: strTeamBadge
-facebook: strFacebook
-twitter: strtwitter
-instagram: strInstagram
-id: idTeam
-country: strCountry
-
-team description: strDescriptionEN
+###
+Know Your Team
+This is a website containing info about teams and their social media links.
+in the home page you will find the team cards. if you click on the explore button then
+you will be taken to the page where you will find some detailed info about the teams.
+in that page, you will find social icon to take you to their social media pages on click.
--- src/components/TeamDetail/TeamDetail.js
+++ src/components/TeamDetail/TeamDetail.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import BannerImage from '../../resources/images/top-banner.jpg';
-import Male from '../../resources/images/male.png';
+import MaleImage from '../../resources/images/male.png';
+import FemaleImage from '../../resources/images/female.png';
 import './TeamDetail.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faRegistered, faFlag, faMars, faFutbol } from '@fortawesome/free-solid-svg-icons'
@@ -19,36 +20,38 @@ const TeamDetail = () => {
             .then(data => setDetails(data.teams[0]));
     }, [id])
 
+    const {strTeamBadge, intFormedYear, strCountry, strSport, strGender, strFacebook, strTwitter, strInstagram} = details;
+
     return (
         <div className="bg-color">
             <div className="container">
                 <div>
                     <img className="img-fluid img" src={BannerImage} alt="" />
+                    <img className="centered" src={strTeamBadge} alt="" />
                 </div>
-
                 <div className="row">
                     <div className="col-md-6">
                         <h1>Name: {details.strTeam}</h1>
-                        <p><FontAwesomeIcon icon={faRegistered} /> Established: {details.intFormedYear}</p>
-                        <p><FontAwesomeIcon icon={faFlag} /> Country: {details.strCountry}</p>
-                        <p><FontAwesomeIcon icon={faFutbol} /> Sport Type: {details.strSport}</p>
-                        <p><FontAwesomeIcon icon={faMars} /> Gender: {details.strGender}</p>
+                        <p><FontAwesomeIcon icon={faRegistered} /> Established: {intFormedYear}</p>
+                        <p><FontAwesomeIcon icon={faFlag} /> Country: {strCountry}</p>
+                        <p><FontAwesomeIcon icon={faFutbol} /> Sport Type: {strSport}</p>
+                        <p><FontAwesomeIcon icon={faMars} /> Gender: {strGender}</p>
                     </div>
                     <div className="col-md-5">
-                        <img className="img-fluid" src={Male} alt="" />
+                        {
+                            strGender === "Male" ? <img className="img-fluid" src={MaleImage} alt="" /> : <img className="img-fluid" src={FemaleImage} alt="" />
+                        }
                     </div>
                 </div>
-
                 <div className="bg-color-blue">
-                    <h3>Team History</h3> <br/>
+                    <h3>Team History</h3> <br />
                     <p>{details.strDescriptionEN}</p>
                 </div>
                 <div className="social-icons">
-                    <a href={details.strFacebook}><FontAwesomeIcon icon={faFacebookSquare} /></a>
-                    <a href={details.strTwitter}><FontAwesomeIcon icon={faTwitterSquare} /></a>
-                    <a href={details.strInstagram}><FontAwesomeIcon icon={faInstagramSquare} /></a>
+                    <a target="_blank" rel="noreferrer" href={'https://'+strFacebook}><FontAwesomeIcon icon={faFacebookSquare} /></a>
+                    <a target="_blank" rel="noreferrer" href={'https://'+strTwitter}><FontAwesomeIcon icon={faTwitterSquare} /></a>
+                    <a target="_blank" rel="noreferrer" href={'https://'+strInstagram}><FontAwesomeIcon icon={faInstagramSquare} /></a>
                 </div>
-
             </div>
         </div>
     );
